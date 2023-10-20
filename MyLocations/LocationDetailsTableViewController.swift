6b20bc3bfecd203b505eb1507b4ee08e75cf8980
€%ER Padilla! <eder.padilla97@gmail.com>
2023-10-20 15:39:20 -0600

    add assets

--- MyLocations/LocationDetailsTableViewController.swift
+++ MyLocations/LocationDetailsTableViewController.swift
@@ -97,7 +97,10 @@ class LocationDetailsViewController: UITableViewController {
     
     // MARK: - Actions
     @IBAction func done() {
-        navigationController?.popViewController(animated: true)
+        guard let mainView = navigationController?.parent?.view
+        else { return }
+        let hudView = HudView.hud(inView: mainView, animated: true)
+        hudView.text = "Tagged"
     }
     
     @IBAction func cancel() {
