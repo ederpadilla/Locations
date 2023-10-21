88b81f7df23a8ad8c7db3dbde5fdc09a7251a8d1
€%ER Padilla! <eder.padilla97@gmail.com>
2023-10-21 17:18:24 -0600

    change code gen to manual

---
+++ LocationEntity+CoreDataClass.swift
@@ -0,0 +1,15 @@
+//
+//  LocationEntity+CoreDataClass.swift
+//  MyLocations
+//
+//  Created by Eder  Padilla on 21/10/23.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(LocationEntity)
+public class LocationEntity: NSManagedObject {
+
+}
---
+++ LocationEntity+CoreDataProperties.swift
@@ -0,0 +1,30 @@
+//
+//  LocationEntity+CoreDataProperties.swift
+//  MyLocations
+//
+//  Created by Eder  Padilla on 21/10/23.
+//
+//
+
+import Foundation
+import CoreData
+
+
+extension LocationEntity {
+
+    @nonobjc public class func fetchRequest() -> NSFetchRequest<LocationEntity> {
+        return NSFetchRequest<LocationEntity>(entityName: "LocationEntity")
+    }
+
+    @NSManaged public var latitude: Double
+    @NSManaged public var longitude: Double
+    @NSManaged public var date: Date?
+    @NSManaged public var locationDescription: String?
+    @NSManaged public var category: String?
+    @NSManaged public var placemark: NSObject?
+
+}
+
+extension LocationEntity : Identifiable {
+
+}
