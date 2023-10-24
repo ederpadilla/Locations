1624e65a9a2c716f779daaa83e830412b95ee79e
€%ER Padilla! <eder.padilla97@gmail.com>
2023-10-24 13:16:41 -0600

    Init support

--- LocationEntity+CoreDataClass.swift
+++ LocationEntity+CoreDataClass.swift
@@ -8,8 +8,25 @@
 
 import Foundation
 import CoreData
+import MapKit
 
 @objc(LocationEntity)
-public class LocationEntity: NSManagedObject {
-
+public class LocationEntity: NSManagedObject, MKAnnotation {
+    
+    public var coordinate: CLLocationCoordinate2D {
+        CLLocationCoordinate2DMake(latitude, longitude)
+    }
+    
+    public var title: String? {
+        if locationDescription.isEmpty {
+            return "(No Description)"
+        } else {
+            return locationDescription
+        }
+    }
+    
+    public var subtitle: String? {
+        category
+    }
+    
 }
