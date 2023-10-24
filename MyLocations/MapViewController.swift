efdde6c3ad84a662ddad7285fd8af50c3f7e2ba3
€%ER Padilla! <eder.padilla97@gmail.com>
2023-10-24 12:52:49 -0600

    connect vcs

--- MyLocations/MapViewController.swift
+++ MyLocations/MapViewController.swift
@@ -6,10 +6,29 @@
 //
 
 import UIKit
+import MapKit
+import CoreData
 
 class MapViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
+    
+    @IBOutlet var mapView: MKMapView!
+    
+    var managedObjectContext: NSManagedObjectContext!
+    
+    // MARK: - Actions
+    @IBAction func showUser() {
+        let region = MKCoordinateRegion(
+            center: mapView.userLocation.coordinate,
+            latitudinalMeters: 1000,
+            longitudinalMeters: 1000)
+        mapView.setRegion(
+            mapView.regionThatFits(region),
+            animated: true)
     }
+    
+    @IBAction func showLocations() {
+    }
+}
+
+extension MapViewController: MKMapViewDelegate {
 }
