acd7a6a18d82e790567366e458efbf77ddaa2a44
€%ER Padilla! <eder.padilla97@gmail.com>
2023-10-24 12:45:59 -0600

    Add vc

---
+++ MyLocations/MapViewController.swift
@@ -0,0 +1,15 @@
+//
+//  MapViewController.swift
+//  MyLocations
+//
+//  Created by Eder  Padilla on 24/10/23.
+//
+
+import UIKit
+
+class MapViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
